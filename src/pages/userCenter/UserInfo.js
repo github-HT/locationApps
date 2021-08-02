b4d318c962bc4b8dd99b584177ed9fa28fe67f9a
ht <wht@jadegreat.com>
2021-08-02 14:38:51 +0800

    Create UserInfo.js

---
+++ src/pages/userCenter/UserInfo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+export default class UserInfo extends Component {
+  render() {
+    return (
+      <View>
+        <Text> 用户中心 </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
