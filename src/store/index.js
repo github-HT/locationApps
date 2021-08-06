fb0ddee584977c1a60704c844f93ab90b2f96967
ht <wht@jadegreat.com>
2021-08-06 13:30:19 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import {combineReducers, createStore} from 'redux';
 import StylesReducer from './reducers/StylesReducer';
 import ThemeReducer from './reducers/ThemeReducer';
+import UserReducer from './reducers/UserReducer';
 const reducers = combineReducers({
   theme: ThemeReducer,
   styles: StylesReducer,
+  user: UserReducer,
 });
 const store = createStore(reducers);
 export default store;
