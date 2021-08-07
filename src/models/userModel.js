43c315519851e6be51eb49b65bd3cc5c1feea9d3
ht <wht@jadegreat.com>
2021-08-07 15:24:24 +0800

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,8 @@
 import {doLogOut, initSignToken} from '../api/api';
 import store from '../store';
 import {UtilStorage} from '../utils/utils';
+import {v5 as uuidv5} from 'uuid';
+import {v1 as uuidv1} from 'uuid';
 
 export function initLocalStorage() {
   initLocalUserInfo();
@@ -34,4 +36,24 @@ export function LogOut(userInfo) {
   store.dispatch({
     type: 'CLEAR_USER_INFO',
   });
+}
+
+export async function initDeviceId(userInfo) {
+  const res = await UtilStorage.getItem('LOCAL_DEVICE_UUID');
+  if (res) {
+    return res;
+  }
+  const v1options = {
+    node: [0x01, 0x23, 0x45, 0x67, 0x89, 0xab],
+    clockseq: 0x1234,
+    msecs: new Date().getTime(),
+    nsecs: Math.floor(Math.random() * 10000),
+  };
+  const uuid = uuidv5(
+    JSON.stringify(v1options),
+    'bba15122-f749-11eb-9234-0123456789ab',
+  );
+  const res1 = await UtilStorage.setItem('LOCAL_DEVICE_UUID', uuid);
+  console.log(uuid, res1);
+  return uuid;
 }
